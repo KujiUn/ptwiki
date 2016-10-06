05b4a3313d23d4c708b0f4effafbb170eae2c16e
KujiUn <toneotom@gmail.com>
2016-10-06 15:44:06 -0700

    Started coding PDSourceReader. First few lines will read properly.

--- pokedex/pdsourcereader.pl
+++ pokedex/pdsourcereader.pl
@@ -3,6 +3,27 @@
 open FILIN "pokedex-source.txt" or die $!;
 open FILOUT ">pokedex.csv" or die $!;
 
+my %Pokemon = {};
+my $mode;
+my @basestats = ();
+
+sub printPokemon {
+
+}
+
 while (my $line = <FILIN>) {
-	
+	chomp $line;
+	if ($line =~ m/^[A-Z ]$/) {
+		printPokemon;
+		$Pokemon{'Name'} = $line;
+	} elsif ($line =~ m/Base Stats/) {
+		$mode = $line;
+	} elsif ($line =~ m/Basic Information/) {
+		$mode = $line;
+		$Pokemon{'Base Stats'} = join(' / ', @basestats);
+		@basestats = ();
+	} elsif ($mode =~ m/Base Stats/) {
+		my ($b1, $b2) = split(/: /,$line);
+		push @basestats, $b2;
+	}
 }
